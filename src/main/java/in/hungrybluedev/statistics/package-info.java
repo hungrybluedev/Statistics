5592205d30fda192c681756695e404dad883e183
HungryBlueDev <hungrybluedev@gmail.com>
2020-02-26 20:27:41 +0530

    Updated package-info for root package statistics

---
+++ src/main/java/in/hungrybluedev/statistics/package-info.java
@@ -0,0 +1,13 @@
+/**
+ * Statistics by Subhomoy Haldar
+ * (<a href="https://twitter.com/hungrybluedev">@HungryBlueDev</a>).
+ * <p>
+ * This package is meant to help people perform basic Statistical tasks
+ * with ease. This includes:
+ * <ol>
+ *     <li>Generating summary statistics.</li>
+ *     <li>Saving collected data in easily accessible formats.</li>
+ *     <li>Creating visualizations and exporting them.</li>
+ * </ol>
+ */
+package in.hungrybluedev.statistics;
